49b6c8798072c17f012127258ce7e29fff375ca4
Vadym Vynokur <king.krollie@gmail.com>
2025-10-09 13:32:05 +0300

    passed some flake8 tests

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 def format_linter_error(error: dict) -> dict:
-    return {"column": error["column_number"], "line": error["line_number"], "message": error["text"], "name": error["code"], "source": "flake8"}
+    return {"column": error["column_number"],
+            "line": error["line_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -9,4 +13,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(rapports, linter_report[rapports]) for rapports in linter_report]
+    return [format_single_linter_file(rapports, linter_report[rapports])
+            for rapports in linter_report]
